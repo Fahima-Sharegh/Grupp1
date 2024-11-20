2441797131b149cdc74d8e207a213becb0c8bb33
Anders Johansson <trumanders@gmail.com>
2024-11-20 16:03:23 +0100

    test again

---
+++ src/test/java/selenium/GmailIT.java
@@ -0,0 +1,47 @@
+package selenium;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GmailIT {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setup() {
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--remote-allow-origins=*");
+        option.addArguments("incognito");
+        driver = new ChromeDriver(option);
+    }
+
+    @Test
+    public void checkTitle() {
+        try {
+            // Navigate to Gmail
+            driver.get("https://www.gmail.com");
+
+            // Get the title of the page
+            String title = driver.getTitle();
+
+            // Assert that the title is correct
+            assertEquals("Gmail", title);
+
+            System.out.println("Test Passed! Title is correct: " + title);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
+    }
+}
--- src/test/java/selenium/RunCucumberIT.java
+++ src/test/java/selenium/RunCucumberIT.java
@@ -1,7 +1,13 @@
 package selenium;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class RunCucumberIT {
     @Test
