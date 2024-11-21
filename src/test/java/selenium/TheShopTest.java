4f3531947c5e05d20d4496cf72af02f410f0df1b
Anders Johansson <trumanders@gmail.com>
2024-11-21 10:33:43 +0100

    Add Webbutiken-tests

--- src/test/java/selenium/TheShopTest.java
+++ src/test/java/selenium/TheShopTest.java
@@ -2,14 +2,16 @@ package selenium;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class GmailIT {
+public class TheShopTest {
     WebDriver driver;
+    String URL = "https://webshop-agil-testautomatiserare.netlify.app/";
 
     @BeforeAll
     static void setupClass() {
@@ -24,28 +26,21 @@ public class GmailIT {
         option.addArguments("--headless");
         option.addArguments("--no-sandbox");
         option.addArguments("--disable-dev-shm-usage");
+
+        // Disable logging for Google Chrome browser
         option.setCapability("goog:loggingPrefs", java.util.Map.of("browser", "OFF"));
         driver = new ChromeDriver(option);
+        driver.get(URL);
     }
 
     @Test
     public void checkTitle() {
-        try {
-            // Navigate to Gmail
-            driver.get("https://www.gmail.com");
+        assertEquals("Webbutiken", driver.getTitle());
+        driver.quit();
+    }
 
-            // Get the title of the page
-            String title = driver.getTitle();
-
-            // Assert that the title is correct
-            assertEquals("Gmail", title);
-
-            System.out.println("Test Passed! Title is correct: " + title);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Close the browser
-            driver.quit();
-        }
+    @Test
+    public void checkAllProductsButton() {
+        assertEquals("All products", driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/button")).getText());
     }
 }
