ad105e1fbaa49ccc164b8b328696a507b0e65117
Carl-Johan Svensson <83705522+Moraxan@users.noreply.github.com>
2024-11-25 18:16:54 +0100

    Develop (#37)
    Merged from development

---
+++ src/test/java/com/example/produktapi/TestTest.java
@@ -0,0 +1,12 @@
+package com.example.produktapi;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestTest {
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+}
