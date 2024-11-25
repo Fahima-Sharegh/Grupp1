36cdf0971024b055f04cd283e735ef2928fee993
Moraxan <83705522+Moraxan@users.noreply.github.com>
2024-11-25 20:16:42 +0100

    Removed unused using and hello world endpoint

--- src/main/java/com/example/produktapi/controller/ProductController.java
+++ src/main/java/com/example/produktapi/controller/ProductController.java
@@ -1,6 +1,4 @@
 package com.example.produktapi.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +15,6 @@ public class ProductController {
 
     public ProductController(ProductService productService) {
         this.productService = productService;
-    }
-
-    @GetMapping("/my-endpoint")
-    ResponseEntity<String> hello(){
-        return new ResponseEntity<String>("Hello, world!", HttpStatus.OK);
     }
     @GetMapping("/products")
     ResponseEntity<List<Product>>getAllProducts() {
